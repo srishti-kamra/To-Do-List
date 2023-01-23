1dc5fa929adbb1ccba09ed3314291cd0a8b8e53c
Srishti Kamra <srishtikamra07@gmail.com>
2023-01-23 09:29:55 -0600

    Initial Commit

---
+++ Assisgnment Notebook/Assisgnment_NotebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assisgnment_NotebookApp.swift
+//  Assisgnment Notebook
+//
+//  Created by Srishti Kamra  on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Assisgnment_NotebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
