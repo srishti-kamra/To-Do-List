a98b7112feb8663ff22142844ab8f72b0a9a6ab5
Srishti Kamra <srishtikamra07@gmail.com>
2023-01-26 09:45:21 -0600

    Enabled data persistance

--- Assisgnment Notebook/AddItemView.swift
+++ Assisgnment Notebook/AddItemView.swift
@@ -1,13 +1,6 @@
-//
-//  AddItemView.swift
-//  Assisgnment Notebook
-//
-//  Created by Srishti Kamra  on 1/24/23.
-//
-
 import SwiftUI
 
-struct AddItemView: View {
+struct AddedItemView: View {
     @ObservedObject var toDoList: ToDoList
     @State private var priority = ""
     @State private var description = ""
@@ -15,23 +8,33 @@ struct AddItemView: View {
     @Environment(\.presentationMode) var presentationMode
     static let priorities = ["High", "Medium", "Low"]
     var body: some View {
-        NavigationView
-        {
+        NavigationView{
             Form {
-                Picker("Priority", selection: $priority){
+                Picker("Priority", selection: $priority) {
                     ForEach(Self.priorities, id: \.self) { priority in
                         Text(priority)
+                        TextField("Description", text: $description)
+                        DatePicker("Due Date", selection: $dueDate, displayedComponents: .date)
                     }
                 }
             }
-            
-            
+            .navigationBarTitle("Add New To-Do Item", displayMode: .inline)
+            .navigationBarItems(trailing: Button("Save") {
+                if priority.count > 0 && description.count > 0 {
+                    let item = ToDoItem(id: UUID(), priority: priority,
+                                        description: description, dueDate : dueDate)
+                    toDoList.items.append(item)
+                    presentationMode.wrappedValue.dismiss()
+                    
+                }
+            })
         }
     }
 }
 
-struct AddItemView_Previews: PreviewProvider {
+
+struct AddedItemView_Previews: PreviewProvider {
     static var previews: some View {
-        AddItemView(toDoList: ToDoList())
+        AddedItemView(toDoList: ToDoList())
     }
 }
--- Assisgnment Notebook/ContentView.swift
+++ Assisgnment Notebook/ContentView.swift
@@ -44,10 +44,11 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct ToDoItem: Identifiable {
-    var id = UUID()
-    var priority = String ()
-    var description = String()
-    var dueDate = Date()
-    
-}
+struct ToDoItem: Identifiable, Codable {
+        var id = UUID()
+        var priority = String ()
+        var description = String()
+        var dueDate = Date()
+        
+    }
+
